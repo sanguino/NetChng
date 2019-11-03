11378033870b5295562b152799b29cccf7c6fb4e
Miguel G. Sanguino <sanguino@me.com>
2019-11-03 22:14:24 +0100

    feat: add start at login

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,9 +1,12 @@
+let AutoLaunch = require('auto-launch');
+
 new Vue({
   el: '#app',
   data: {
     adaptersList: [],
     section: 0,
-    selectedAdapters: []
+    selectedAdapters: [],
+    startOnLogin: false
   },
 
   methods: {
@@ -12,7 +15,25 @@ new Vue({
     },
   },
 
-  beforeMount() {
+  watch: {
+    startOnLogin: async function (val) {
+      val ? this.autoLaunch.enable() : this.autoLaunch.disable();
+    },
+  },
+
+  async beforeMount() {
+
+    this.autoLaunch = new AutoLaunch({
+      name: 'NetChng',
+      path: process.execPath,
+      mac: {
+        useLaunchAgent: true
+      }
+    });
+
+    this.startOnLogin = await this.autoLaunch.isEnabled();
+    console.log(' this.autoLaunch.isEnabled()', this.startOnLogin)
+
     require('electron').ipcRenderer.on('data', (event, data) => {
       this.adaptersList = data.adaptersList;
     });
